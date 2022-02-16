a45f6fd701f1db2af0d86622debe4d3bf0a8aeae
Namae Conde <lunamae.conde@sensis.com.au>
2022-02-17 02:20:47 +1100

    Add dark theme to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { darkTheme } from './theme';
 
+/**
+ * Application is default dark themed
+ */
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <App/>
+  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
