6b097c0eb1c7807ecbcac5a558470dc14d0e6906
Namae Conde <lunamae.conde@sensis.com.au>
2022-02-19 02:46:18 +1100

    Add hasRover indicator

--- src/components/Terrain.tsx
+++ src/components/Terrain.tsx
@@ -2,20 +2,28 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import brown from '@mui/material/colors/brown';
+import ElectricCarIcon from '@mui/icons-material/ElectricCar';
 
 const Terrain = ({ width, height }: any) => {
-  const Block = () => {
+  const Block = ({ hasRover }: any) => {
     return (
       <Paper 
         sx={{
           m: 0.2,
           width: 30,
           height: 30,
-          backgroundColor: brown[500]
+          backgroundColor: brown[500],
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
         }}
         elevation={3}
         square
-      />
+      >
+        {
+          hasRover && <ElectricCarIcon />
+        }  
+      </Paper>
     )
   }
 
