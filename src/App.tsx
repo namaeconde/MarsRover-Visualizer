9248f441f9d3116a64a5124b62de2d9a7ccd1c5c
Namae Conde <lunamae.conde@sensis.com.au>
2022-02-19 02:26:11 +1100

    Add terrain to App with default width and height

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Terrain from './components/Terrain';
+
+const DEFAULT_WIDTH = 5
+const DEFAULT_HEIGHT = 5
 
 const App = () => {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-          >
-            Plateau
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <>
+      <CssBaseline />
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+            >
+              Mars Rover Operation
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Terrain width={DEFAULT_WIDTH} height={DEFAULT_HEIGHT} />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { darkTheme } from './theme';
 
@@ -11,8 +10,7 @@ import { darkTheme } from './theme';
  */
 ReactDOM.render(
   <React.StrictMode>
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
+  <ThemeProvider theme={darkTheme}>    
     <App/>
   </ThemeProvider>
   </React.StrictMode>,
