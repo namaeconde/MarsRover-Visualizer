53373081b452921b94a58a7e5745fb87f2ff9feb
Namae Conde <lunamae.conde@sensis.com.au>
2022-02-19 16:45:54 +1100

    Update eq javascript warning

--- src/domain/Plateau.tsx
+++ src/domain/Plateau.tsx
@@ -21,8 +21,8 @@
      hasRoverAt(position: Point): boolean {
          return this.rovers.some((rover) => {
              let roverPosition = rover.position;
-             return position.x == roverPosition.x &&
-                 position.y == roverPosition.y
+             return position.x === roverPosition.x &&
+                 position.y === roverPosition.y
          })
      }
  }
